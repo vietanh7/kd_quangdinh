c5879f74d5e1d71d3a5cdcad701a710dbe2a28f6
quangtuongdinh116 <quangtuongdinh116@gmail.com>
2022-03-02 10:06:16 +0700

    Create Repository.kt

---
+++ app/src/main/java/com/example/android/kdquangdinh/data/Repository.kt
@@ -0,0 +1,11 @@
+package com.example.android.kdquangdinh.data
+
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class Repository @Inject constructor(
+    remoteDataSource: RemoteDataSource,
+) {
+    val remote = remoteDataSource
+}
