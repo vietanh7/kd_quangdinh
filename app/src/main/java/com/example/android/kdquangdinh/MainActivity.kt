2cda4664bacb90048f604235a8ef793fcedb1d62
quangtuongdinh116 <quangtuongdinh116@gmail.com>
2022-03-02 09:12:46 +0700

    Create Nav Graph and set up it in MainActivity

--- app/src/main/java/com/example/android/kdquangdinh/MainActivity.kt
+++ app/src/main/java/com/example/android/kdquangdinh/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.android.kdquangdinh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.example.android.kdquangdinh.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.navHostFragment)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
