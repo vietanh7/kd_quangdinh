83ee6b573ef2b55eed72dd0893ca77276dad77c6
quangtuongdinh116 <quangtuongdinh116@gmail.com>
2022-03-02 14:10:12 +0700

    Update ProductsAdapter.kt

--- app/src/main/java/com/example/android/kdquangdinh/adapters/ProductsAdapter.kt
+++ app/src/main/java/com/example/android/kdquangdinh/adapters/ProductsAdapter.kt
@@ -1,16 +1,115 @@
 package com.example.android.kdquangdinh.adapters
 
-class ProductsAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.kdquangdinh.R
+import com.example.android.kdquangdinh.databinding.ProductRowLayoutBinding
+import com.example.android.kdquangdinh.models.Product
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private val ITEM_VIEW_TYPE_HEADER = 0
+private val ITEM_VIEW_TYPE_ITEM = 1
+
+class ProductsAdapter(val clickListener: ProductListener) :
+    ListAdapter<DataItem, RecyclerView.ViewHolder>(ProductDiffCallback()){
+
+    private val adapterScope = CoroutineScope(Dispatchers.Default)
+
+    fun addHeaderAndSubmitList(list: List<Product>?) {
+        adapterScope.launch {
+            val items = when (list) {
+                null -> listOf(DataItem.Header)
+                else -> listOf(DataItem.Header) + list.map { DataItem.ProductItem(it) }
+            }
+            withContext(Dispatchers.Main) {
+                submitList(items)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is DataItem.Header -> ITEM_VIEW_TYPE_HEADER
+            is DataItem.ProductItem -> ITEM_VIEW_TYPE_ITEM
+        }
+    }
+
+    class HeaderHolder(view: View): RecyclerView.ViewHolder(view) {
+        companion object {
+            fun from(parent: ViewGroup): HeaderHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.header_row_layout, parent, false)
+                return HeaderHolder(view)
+            }
+        }
+    }
+
+    class ViewHolder private constructor(val binding: ProductRowLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(item: Product, clickListener: ProductListener) {
+            binding.sleep = item
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ProductRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ITEM_VIEW_TYPE_HEADER -> HeaderHolder.from(parent)
+            ITEM_VIEW_TYPE_ITEM -> ViewHolder.from(parent)
+            else -> throw ClassCastException("Unknown viewType ${viewType}")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolder -> {
+                val productItem = getItem(position) as DataItem.ProductItem
+                holder.bind(productItem.product, clickListener)
+            }
+        }
+    }
+}
+
+class ProductDiffCallback : DiffUtil.ItemCallback<DataItem>() {
+    override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+        return oldItem.id == newItem.id
+    }
+    @SuppressLint("DiffUtilEquals")
+    override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class ProductListener(val updateListener: (productId: Long?) -> Unit, val removeListener: (productId: Long?) -> Unit) {
+    fun onUpdateClick(product: Product) = updateListener(product.id)
+    fun onRemoveClick(product: Product) = removeListener(product.id)
 }
 
 sealed class DataItem {
-    data class ProductItem(val sleepNight: SleepNight): DataItem() {
-        override val id = sleepNight.nightId
+    data class ProductItem(val product: Product): DataItem() {
+        override val id = product.id
     }
 
     object Header: DataItem() {
         override val id = Long.MIN_VALUE
     }
 
-    abstract val id: Long
+    abstract val id: Long?
 }
